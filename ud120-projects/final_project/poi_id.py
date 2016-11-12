781782b965f4f5579abdc414fa18ba8a06c81ead
Sunil Ranka <sunil.ranka@jgus-2k16-MC4.local>
2016-11-11 18:40:49 -0800

    initial

--- ud120-projects/final_project/poi_id.py
+++ ud120-projects/final_project/poi_id.py
@@ -8,7 +8,7 @@ from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
 ###### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
+##### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
 features_list = ['poi','salary'] # You will need to use more features
 
